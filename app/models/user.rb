8e274b28167a07cfd823ccaf65492bf35883b913
PatriciodelaGuardia <delaguardiapatricio@gmail.com>
2016-01-28 15:20:44 -0500

    Add Sendgrid for production mailing and gmail for development

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,6 @@ class User < ActiveRecord::Base
   validate :email_is_unique, on: :create
   after_create :create_account
 
-  #To be deleted after mail validation
-  def confirmation_required?
-    false
-  end
-
   private
 
   def email_is_unique
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Configuaration for Devise
+  config.action_mailer.default_url_options = { :host => 'weather-app-1501.herokuapp.com' }
 end
