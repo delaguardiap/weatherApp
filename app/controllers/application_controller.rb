80a0a2a41dda8e09519adf5c8636dff0c01b6d36
PatriciodelaGuardia <delaguardiapatricio@gmail.com>
2016-01-28 11:24:46 -0500

    Add before_action callback on ApplicationController to restrict access to unauthenticated users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate_user!, except: [:new_usser_session, :new_user_registration, :user_registration]
 end
